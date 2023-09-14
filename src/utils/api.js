08290ba2b0e0892a0770b15907cd0936625c6ea5
Saif <saif49032@gmail.com>
2023-09-14 12:09:29 +0530

    .env create for security

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 const BASE_URL = "https://www.googleapis.com/customsearch/v1";
 
 const params = {
-  key: "AIzaSyDRTWc5gyysD8WfSQsSCm2cRszxw027hMs",
-  cx: "13817b869d0074e95",
+  key: import.meta.env.VITE_SOME_KEY_ONE,
+  CX: import.meta.env.VITE_SOME_KEY_TWO,
 };
 export const fetchDataFromApi = async (payload) => {
   const { data } = await axios.get(BASE_URL, {
